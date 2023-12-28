cf6e4a18eeff2762d134ade552521df435a4871c
linuxpedro <pdro.dev97@gmail.com>
2023-12-27 22:26:29 -0300

    feat: new component

---
+++ learnings/1components/src/Components/DynamicTitle.js
@@ -0,0 +1,26 @@
+class DynamicTitle extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open" });
+
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = 'Pedro'
+        //component base
+
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `;  
+        //component style
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+        //send to shadow
+
+    }
+}
+
+customElements.define('dynamic-title', DynamicTitle);
