a7a563558b743fc2c25ca0bbed02f04cc3be2aa3
linuxpedro <pdro.dev97@gmail.com>
2023-12-27 21:31:34 -0300

    fix: header

--- learnings/1components/cardFile.js
+++
@@ -1,9 +0,0 @@
-class CardNews extends HTMLElement {
-  constructor() {
-    super(); //method used to call a method in a parent class (HTMLElement)
-    const shadow = this.attachShadow({mode: "open" })
-    shadow.innerHTML = "<h1>Hello World</h1>"
-  }
-}
-
-customElements.define('card-news', CardNews)
--- learnings/1components/src/Components/CardNews.js
+++ learnings/1components/src/Components/CardNews.js
@@ -1,3 +1,11 @@
-class CardNews HTMLElement {
-  _constructor
+class CardNews extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    shadow.innerHTML = "<h1>Hello World</h1>"
+  }
 }
+
+customElements.define('card-news', CardNews)
